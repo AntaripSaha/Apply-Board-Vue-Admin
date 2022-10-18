01fbebd9b0df83cc49266154815644692d1afb42
Antarip Saha <antarip15@gmail.com>
2022-10-18 10:55:57 +0600

    navbar updated

--- src/navigation/vertical/dashboard.js
+++ src/navigation/vertical/dashboard.js
@@ -5,4 +5,62 @@ export default [
     icon: 'HomeIcon',
     tagVariant: 'light-warning',
   },
+  {
+    title: 'Employee',
+    icon: 'UsersIcon',
+    children: [
+      {
+        title: 'List',
+        route: 'apps-users-list',
+      },
+      {
+        title: 'View',
+        route: { name: 'apps-users-view', params: { id: 21 } },
+      },
+      {
+        title: 'Edit',
+        route: { name: 'apps-users-edit', params: { id: 21 } },
+      },
+    ],
+  },
+  {
+    title: 'University',
+    icon: 'BookOpenIcon',
+    children: [
+      {
+        title: 'List',
+        route: 'apps-invoice-list',
+      },
+      {
+        title: 'Preview',
+        route: { name: 'apps-invoice-preview', params: { id: 4987 } },
+      },
+      {
+        title: 'Edit',
+        route: { name: 'apps-invoice-edit', params: { id: 4987 } },
+      },
+      {
+        title: 'Add',
+        route: { name: 'apps-invoice-add' },
+      },
+    ],
+  },
+  {
+    title: 'Student',
+    icon: 'UserIcon',
+    children: [
+      {
+        title: 'List',
+        route: 'apps-users-list',
+      },
+      {
+        title: 'View',
+        route: { name: 'apps-users-view', params: { id: 21 } },
+      },
+      {
+        title: 'Edit',
+        route: { name: 'apps-users-edit', params: { id: 21 } },
+      },
+    ],
+  },
 ]
